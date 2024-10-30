877576ad0b366432fa3708926ab17808d2606213
JuxGD <jak@e.email>
2024-10-29 23:33:54 -0300

    PLEASE

--- configuration.nix
+++ configuration.nix
@@ -5,8 +5,8 @@
 { inputs, config, lib, pkgs, ... }:
 
 let
-  staging = inputs.staging.packages.${lib.system}.pkgs;
-  stable = inputs.stable.packages.${lib.system}.pkgs;
+  staging = inputs.staging.nixpkgs.${lib.system}.pkgs;
+  stable = inputs.stable.nixpkgs.${lib.system}.pkgs;
 
   doubleclickingtext = ''
     [Never Debounce]
