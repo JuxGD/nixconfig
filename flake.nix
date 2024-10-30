afcdeb3385159b5ef6a85a649ab8c95d79973f98
JuxGD <jak@e.email>
2024-10-30 00:51:42 -0300

    I LITERALLY COPIED ANOTHER NIX CONFIG. PLEASE WORK

--- flake.nix
+++ flake.nix
@@ -2,26 +2,40 @@
   description = "NixOS configuration";
 
   inputs = {
-    stable = { url = "github:NixOS/nixpkgs/nixos-23.11"; };
-    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-unstable"; };
-    staging = { url = "github:NixOS/nixpkgs/staging"; };
-    musnix = { url = "github:musnix/musnix"; };
+    stable.url = "nixpkgs/24.05";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    staging.url = "nixpkgs/staging";
+    musnix.url = "github:musnix/musnix";
   };
 
-  outputs = { self, stable, nixpkgs, staging, ... }@inputs: rec {
-    
+  outputs = { self, stable, nixpkgs, staging, ... }@inputs:
+  let
+    overlay = final: prev: let
+      stablePkgs = import stable { inherit (prev) system; config.allowUnfree = true; };
+      stagingPkgs = import staging { inherit (prev) system; config.allowUnfree = true; };
+    in {
+      stable = stablePkgs;
+      zhdStable = stablePkgs.zfsStable;
+      staging = stagingPkgs;
+      zhdStaging = stagingPkfs.zfsStaging;
+    };
+
+    overlayModule = ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay ]; });
+  in
+  rec {
     nixosConfigurations = {
       jpc = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           inputs.musnix.nixosModules.musnix
+          overlayModule
           ./configuration.nix
           ./packages.nix
           ./graphics.nix
           ./audio.nix
           ./fonts.nix
         ];
-      specialArgs = { inherit inputs; inherit stable; inherit staging; };
+        specialArgs.channels = { inherit nixpkgs stable staging; };
       };
     };
   };
