da0f7e999226be5d0d1c4862ba7f0bea8537a5da
JuxGD <jak@e.email>
2024-11-25 18:27:56 -0300

    BRUH i forgot how lists work

--- graphics.nix
+++ graphics.nix
@@ -65,7 +65,7 @@ in
       {
         name = "NVIDIA patch for Linux 6.12";
         patch = (builtins.fetchurl https://github.com/Binary-Eater/open-gpu-kernel-modules/commit/8ac26d3c66ea88b0f80504bdd1e907658b41609d.patch);
-      };
+      }
     ];
   };
 
