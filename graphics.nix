0132696f3de2dce0b282724b557cec44dd432dc2
JuxGD <jak@e.email>
2024-11-25 18:40:01 -0300

    stand by i'm not done yet

--- graphics.nix
+++ graphics.nix
@@ -64,7 +64,10 @@ in
     kernelPatches = [
       {
         name = "NVIDIA patch for Linux 6.12";
-        patch = (builtins.fetchurl https://github.com/Binary-Eater/open-gpu-kernel-modules/commit/8ac26d3c66ea88b0f80504bdd1e907658b41609d.patch);
+        patch = (builtins.fetchurl {
+          url = "https://github.com/Binary-Eater/open-gpu-kernel-modules/commit/8ac26d3c66ea88b0f80504bdd1e907658b41609d.patch";
+          hash = "";
+        });
       }
     ];
   };
