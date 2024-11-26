29265eab203ccc7003870f2601b842f8d0167359
JuxGD <jak@e.email>
2024-11-26 00:50:40 -0300

    forgot a semicolon once again

--- graphics.nix
+++ graphics.nix
@@ -33,7 +33,7 @@ in
 
   nix.settings = {
     substituters = [ "https://hyprland.cachix.org" ];
-    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ]
+    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
   };
 
   programs = {
