819bbccaba5c9f255b8429b0a073dfce6e6fe78d
JuxGD <jak@e.email>
2025-01-03 01:32:09 -0300

    missed a quotation mark

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
 
     # use lix
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-2.tar.gz;
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-2.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
