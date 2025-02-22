da69e1869f82f29b745e23f82e3f9cf3fe40565f
JuxGD <jak@e.email>
2025-02-21 23:47:50 -0300

    add passthrough support

--- virtualisation.nix
+++ virtualisation.nix
@@ -1,4 +1,10 @@
-{ config, lib, pkgs, inputs, ... }:
+let
+  # RTX 3070 Ti
+  gpuIDs = [
+    "10de:24a0" # Graphics
+    "10de:228b" # Audio
+  ];
+in { config, lib, pkgs, inputs, ... }:
 
 let
   stable = inputs.stable.legacyPackages.${pkgs.system};
@@ -21,8 +27,40 @@ in
       enable = true;
       qemu.vhostUserPackages = with pkgs; [ virtiofsd ];
     };
+    spiceUSBRedirection = true;
 
     waydroid.enable = true;
+  };
+
+  options.vfio.enable = with lib;
+    mkEnableOption "Configure the machine for VFIO";
+  
+  config = let cfg = config.vfio; {
+    in {
+      boot = {
+        initrd.kernelModules = [
+          "vfio_pci"
+          "vfio"
+          "vfio_iommu_type1"
+          "vfio_virqfd"
+
+          "nvidia"
+          "nvidia_modeset"
+          "nvidia_uvm"
+          "nvidia_drm"
+        ];
+
+        kernelParams = [
+          "amd_iommu=on"
+        ]; ++ lib.optional cfg.enable
+          # Isolate the GPU
+          ("vfio-pci.ids=" + lib.concatStrginsSep "," gpuIDs);
+      };
+      specialisation."VFIO".configuration = {
+        system.nixos.tags = [ "with-vfio" ];
+        vfio.enable = true
+      };
+    };
   };
   
   programs = {
