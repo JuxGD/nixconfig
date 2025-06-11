7e91efd1df762895404f20f8d5fea1a0cf4c5dda
JuxGD <jak@e.email>
2025-06-10 22:37:57 -0400

    Revert "typo"
    This reverts commit 707e84f3654b415b7b91ff52a65ce4d0373f9f01.

--- virtualisation.nix
+++ virtualisation.nix
@@ -59,7 +59,7 @@ in
           "amd_iommu=on"
         ] ++ lib.optional cfg.enable
           # Isolate the GPU
-          ("vfio-pci.ids=" + lib.concatStringsSep "," gpuIDs);
+          ("vfio-pci.ids=" + lib.concatStrginsSep "," gpuIDs);
       };
       specialisation."VFIO".configuration = {
         system.nixos.tags = [ "with-vfio" ];
