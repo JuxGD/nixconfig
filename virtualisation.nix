7d11e719aa912ffa20e739af61a43928e76829a4
JuxGD <jak@e.email>
2025-06-10 20:54:47 -0400

    oops forgot to add virt manager

--- virtualisation.nix
+++ virtualisation.nix
@@ -28,6 +28,8 @@ in
         qemu.vhostUserPackages = with pkgs; [ virtiofsd ];
       };
 
+      programs.virt-manager.enable = true;
+
       spiceUSBRedirection.enable = true;
 
       waydroid.enable = true;
