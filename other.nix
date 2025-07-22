993b8183c8b621e1f39e37208efdcf5a43ac5fd3
JuxGD <jak@e.email>
2025-07-22 01:04:32 -0400

    please PLEASE please 2 electric boogaloo

--- other.nix
+++ other.nix
@@ -14,23 +14,23 @@ in
       enable = true;
       acceleration = "cuda";
     };
-
-    programs.obs-studio = {
-      enable = true;
-      package = (
-        pkgs.obs-studio.override {
-          cudaSupport = true;
-        }
-      );
-
-      plugins = with pkgs.obs-studio-plugins; [
-        wlrobs
-        obs-backgroundremoval
-        obs-pipewire-audio-capture
-        obs-vaapi
-        obs-gstreamer
-        obs-vkcapture
-      ];
-    };
   };
+  programs.obs-studio = {
+    enable = true;
+    package = (
+      pkgs.obs-studio.override {
+        cudaSupport = true;
+      }
+    );
+
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-vaapi
+      obs-gstreamer
+      obs-vkcapture
+    ];
+  };
+
 }
