7018c0dd570136e5d1cc4207091f4a9d0dd1ad8d
Jux <jak@e.email>
2025-09-04 21:24:57 -0400

    tlp is useless to me actually i want my ppd back

--- hardware.nix
+++ hardware.nix
@@ -38,29 +38,5 @@ in
       xkb.layout = "us";
       xkb.variant = "";
     };
-
-    power-profiles-daemon.enable = false;
-
-    tlp = {
-      enable = true;
-      settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-	CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
-	CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-	CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-
-	CPU_MIN_PERF_ON_AC = 0;
-	CPU_MAX_PERF_ON_AC = 100;
-	CPU_MIN_PERF_ON_BAT = 0;
-	CPU_MAX_PERF_ON_BAT = 20;
-
-	START_CHARGE_THRESH_BAT1 = 15;
-        STOP_CHARGE_THRESH_BAT1 = 80;
-
-        AMDGPU_ABM_LEVEL_ON_AC = 0;
-	AMDGPU_ABM_LEVEL_ON_BAT = 0;
-      };
-    };
   };
 }
