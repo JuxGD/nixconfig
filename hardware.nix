d3ef038f74a158dd12356bdfd5976a623cc96daf
Jux <jak@e.email>
2025-11-29 21:23:20 -0300

    hopefully this makes the printer work

--- hardware.nix
+++ hardware.nix
@@ -32,6 +32,12 @@ in
 
   services = {
     printing.enable = true;
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
+
     libinput.enable = true;
 
     xserver = {
