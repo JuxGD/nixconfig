e1285bef3ef6ca35dfc7d3ecc59d5c9ee679f169
Jux <jak@e.email>
2026-05-06 21:46:32 -0400

    just checked it doesn't exist why

--- boot.nix
+++ boot.nix
@@ -12,9 +12,6 @@ in
     nix-cachyos-kernel.overlays.default
   ];
 
-  nix.settings.substituters = [ "https://attic.xuyh0120.win/lantian" ];
-  nix.settings.trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
-  
   boot = {
     supportedFilesystems = [ "ntfs" "ext4" ];
     loader = {
