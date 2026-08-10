ec88ae64c81a50e4a685d03b6788ace7760703f6
Jux <jak@e.email>
2026-08-10 11:24:41 -0400

    goodbye sodiboo niri-flake

--- flake.nix
+++ flake.nix
@@ -40,7 +40,7 @@
     };
 
     niri = {
-      url = "github:sodiboo/niri-flake";
+      url = "github:epireyn/niri-flake";
       inputs.niri-unstable.follows = "niri-package";
       inputs.nixpkgs.follows = "nixpkgs";
     };
